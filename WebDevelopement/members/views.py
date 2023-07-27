19d592de2b9d39e9c19a71a47d9ba8d9304c2c95
Abolfazl <dahaghaynhmh@gmail.com>
2023-07-27 15:11:22 +0330

    Adding Files

---
+++ WebDevelopement/members/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from members.models import Members
+from django.conf import settings
+from django.core.mail import send_mail
+
+mymembers = Members.objects.all().values()
+
+def HelloWorldPage(request):
+    return HttpResponse("Bonjour World!")
+def Home(request):
+    #send_mail("Hello World","this is a test","decodenotifier@gamil.com",["decodenotifier@gamil.com"],fail_silently=False)
+    return render(request,'template.html')
+def Members(request):
+    return render(request,'member.html',{'mymembers':mymembers})
