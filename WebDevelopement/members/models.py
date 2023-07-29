f902c3cf0655e31fc28a6ca1602df6739cfb41f9
Abolfazl <dahaghaynhmh@gmail.com>
2023-07-29 17:19:56 +0330

    Test Commit message

--- WebDevelopement/members/models.py
+++ WebDevelopement/members/models.py
@@ -4,7 +4,7 @@ class Members(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     phone_number = models.IntegerField(null=True)
-    about = models.CharField(null=True,max_length=5000) 
+    about = models.TextField(null=True,blank=True,help_text="Input some text about member...")
 
     def __str__(self):
         return f'{self.firstname} {self.lastname}'
