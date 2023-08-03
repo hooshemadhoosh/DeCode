2cea8fe415ae3a62d95b2112dfbc1ac01383c277
IMAN <95289929+I-MA-N@users.noreply.github.com>
2023-08-03 16:52:51 +0330

    404 test

--- WebDevelopement/WebDevelopement/settings.py
+++ WebDevelopement/WebDevelopement/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mci-a)9+-2l&)$eki^q(t^c+4sij0ujx(hak#bkc01ek6ltf!p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
